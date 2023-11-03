460f6320857fdc93fc5c52a687ceac1bd4ad3062
Magda Sułkowska <msulkowska0@gmail.com>
2023-11-03 13:42:31 +0100

    numeracja

--- script.js
+++ script.js
@@ -1,39 +1,82 @@
 var selectedElement = null;
-var currentPlayer = 'white';
+var currentPlayer = 1;
+var chessboard = [
+    [2,2,2,2],
+    [2,2,2,2],
+    [2,2,2,2],
+    [0,0,0,0],
+    [0,0,0,0],
+    [1,1,1,1],
+    [1,1,1,1],
+    [1,1,1,1]
+];
 
 function select(element) 
 {
-    if (selectedElement == null) 
+    if (isAnyElementSelected()) 
     {
-        if (element.querySelector("img[src]") && element.querySelector("img[src]").alt.includes(currentPlayer)) 
+        if (canSelectElement(element))
         {
-            element.style.backgroundColor = "grey";
-            selectedElement = element;
+            currentlySelectedElement(element)
         }
     }
-     else if (element == selectedElement) 
-    
+     else if (isElementSelectedElement(element))     
     {
-        selectedElement.style.backgroundColor = "black";
-        selectedElement = null;
+        unselectSelectedElement();
     } 
-    else if (element.querySelector("img[src]") && element.querySelector("img[src]").alt != (currentPlayer)) 
+    else if (cellContents(element))
     {
         return;
     }
      else 
     {
-        element.appendChild(selectedElement.querySelector("img[src]"));
-        selectedElement.style.backgroundColor = "black";
-        selectedElement = null;
-
-        if (currentPlayer == 'white') 
-        {
-            currentPlayer = 'black';
-        } 
-        else
-        {
-            currentPlayer = 'white';
-        }
+        movePieceToNewLocation(element)
     }
+}
+
+
+
+function isAnyElementSelected()
+{
+    return selectedElement == null;
+}
+
+function canSelectElement(element)
+{
+ return (chessboard[element.dataset.y][element.dataset.x] == currentPlayer)
+}
+
+function currentlySelectedElement(element)
+{
+    element.style.backgroundColor = "grey";
+    selectedElement = element;
+}
+
+function isElementSelectedElement(element)
+{
+    return element == selectedElement;
+}
+
+function unselectSelectedElement()
+{
+    selectedElement.style.backgroundColor = "black";
+    selectedElement = null;
+}
+
+function cellContents(element)
+{
+    return (chessboard[element.dataset.y][element.dataset.x] > 0)
+}
+
+function movePieceToNewLocation(element)
+{
+    chessboard[element.dataset.y][element.dataset.x] = currentPlayer;
+    chessboard[selectedElement.dataset.y][selectedElement.dataset.x] = 0;
+
+    element.appendChild(selectedElement.querySelector("img[src]"));
+    selectedElement.style.backgroundColor = "black";
+    selectedElement = null;
+
+    
+    currentPlayer = 3 - currentPlayer;
 }
