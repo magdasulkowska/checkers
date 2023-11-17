ac31eb9dfaba641049a6e789309eab8c3c63997d
Magda Sułkowska <msulkowska0@gmail.com>
2023-11-17 13:35:45 +0100

    sprawdzanie czy pionek jest do zbicia

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var chessboard = [
     [2,2,2,2],
     [2,2,2,2],
     [0,0,0,0],
-    [0,0,0,0],
+    [0,0,0,0], 
     [1,1,1,1],
     [1,1,1,1],
     [1,1,1,1]
@@ -17,20 +17,28 @@ function select(element)
     {
         if (canSelectElement(element))
         {
-            currentlySelectedElement(element)
+            changeSelectedElement(element)
         }
     }
      else if (isElementSelectedElement(element))     
     {
         unselectSelectedElement();
     } 
-    else if (cellContents(element))
+    else if (containsPawn(element))
     {
         return;
     }
-     else 
+     else if(isGoodMoveForOddRow(element))
     {
-        movePieceToNewLocation(element)
+        makeMoveIfGood(element)
+    }
+    else if (isGoodMoveForEvenRow(element))
+    {
+        makeMoveIfGood(element)
+    }
+    else if(canDeletOpponent(element))
+    {
+        deleteOpponent(element);
     }
 }
 
@@ -46,7 +54,7 @@ function canSelectElement(element)
  return (chessboard[element.dataset.y][element.dataset.x] == currentPlayer)
 }
 
-function currentlySelectedElement(element)
+function changeSelectedElement(element)
 {
     element.style.backgroundColor = "grey";
     selectedElement = element;
@@ -63,9 +71,31 @@ function unselectSelectedElement()
     selectedElement = null;
 }
 
-function cellContents(element)
+function containsPawn(element)
 {
     return (chessboard[element.dataset.y][element.dataset.x] > 0)
+}
+
+function isGoodMoveForOddRow(element)
+{
+   return selectedElement.dataset.y % 2>0 && (selectedElement.dataset.x == element.dataset.x || element.dataset.x == parseInt(selectedElement.dataset.x) +1)
+}
+
+function makeMoveIfGood(element)
+{
+    if (currentPlayer == 1 && selectedElement.dataset.y == parseInt(element.dataset.y) +1)
+        {
+            movePieceToNewLocation(element)
+        }
+        else if (currentPlayer == 2 && selectedElement.dataset.y == parseInt(element.dataset.y) -1)
+        {
+            movePieceToNewLocation(element)
+        }
+}
+
+function isGoodMoveForEvenRow(element)
+{
+    return selectedElement.dataset.y % 2==0 && (selectedElement.dataset.x == element.dataset.x || element.dataset.x == parseInt(selectedElement.dataset.x) -1)
 }
 
 function movePieceToNewLocation(element)
@@ -80,3 +110,30 @@ function movePieceToNewLocation(element)
     
     currentPlayer = 3 - currentPlayer;
 }
+
+
+function canDeletOpponent(element) 
+{
+    var againstPlayer = 3 - currentPlayer;
+    var step = 2 - currentPlayer;
+    var targetY = parseInt(selectedElement.dataset.y) + step;
+    var targetXLeft = parseInt(selectedElement.dataset.x) - 1;
+    var targetXRight = parseInt(selectedElement.dataset.x) + 1;
+
+    function isCorrectCoordinate(y, x) 
+    {
+        return y >= 0 && y < chessboard.length && x >= 0 && x < chessboard[0].length;
+    }
+
+    return (isCorrectCoordinate(targetY + step, targetXLeft) && chessboard[targetY + step][targetXLeft] == againstPlayer || isCorrectCoordinate(targetY + step, targetXRight) && chessboard[targetY + step][targetXRight] == againstPlayer);
+}
+
+function deleteOpponent(element)
+{
+
+}
+
+
+
+
+
